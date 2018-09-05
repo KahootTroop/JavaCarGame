55257e6e5c751bd41e4e31e56ab4cdc8a233a411
Rocky Mazorow <rmazorow@stech.edu>
2018-09-05 08:39:12 -0600

    Initial commit

---
+++ RaceCarPane.java
@@ -0,0 +1,79 @@
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+
+public class RaceCarPane extends Pane {
+	private double x = 15.0;
+	private double y = 15.0;
+	private double width = 0.0;;
+	private double height = 0.0;
+	private Polygon top;
+	private Rectangle body;
+	private Circle w1;
+	private Circle w2;
+	private Color topColor = Color.RED;
+	private Color bodyColor = Color.rgb(250, 128, 144);
+	private Color wheelColor = Color.BLUE;
+	public boolean isOver = false;
+	public boolean didWin = false;
+	
+	public RaceCarPane(int w, int h) {
+		this.y = h;
+		this.width = w;
+		this.height = h;
+		this.draw();
+	}
+	
+	public RaceCarPane(int w, int h, Color t, Color b, Color ws) {
+		this.y = h;
+		this.width = w;
+		this.height = h;
+		this.topColor = t;
+		this.bodyColor = b;
+		this.wheelColor = ws;
+		this.draw();
+	}
+	
+	private void draw() {
+		top = new Polygon();
+		top.getPoints().addAll(new Double[]{
+			x+10, y-20,
+			x+20, y-30,
+			x+30, y-30,
+			x+40, y-20 });
+		top.setFill(topColor);
+		body = new Rectangle(x, y-20, 50, 10);
+		body.setFill(bodyColor);
+		w1 = new Circle(x+15, y-5, 5);
+		w1.setFill(wheelColor);
+		w2 = new Circle(x+35, y-5, 5);
+		w2.setFill(wheelColor);
+		this.getChildren().addAll(top, body, w1, w2);
+	}
+	
+	public void driveForward() {		
+		x += 15;
+		if (x > this.width) {
+			x -= 15;
+			isOver = true;
+			didWin = true;
+			return;
+		}
+		this.draw();
+		this.getChildren().clear();
+		this.getChildren().addAll(top, body, w1, w2);
+	}
+	
+	public void driveBackward() {
+		x -= 15;
+		if (x < 0) {
+			x +=15;
+			isOver = true;
+			didWin = false;
+			return;
+		}
+		this.draw();
+		this.getChildren().clear();
+		this.getChildren().addAll(top, body, w1, w2);
+	}
+}
